348e4072ab3ecd339bdd8a904ba1036349468e20
Cristian Camilo Morales tapias <criscamota199890@gmail.com>
2024-12-27 23:05:02 +0100

    Sames as previous commit

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -43,7 +43,19 @@ require("lazy").setup({
     ---@module "ibl"
     ---@type ibl.config
     opts = {},
-    }
+    },
+    {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 
   },
   -- Configure any other settings here. See the documentation for more details.
