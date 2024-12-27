86e51be83cc644477d1966faa54072bd252fced4
Cristian Camilo Morales tapias <criscamota199890@gmail.com>
2024-12-27 23:05:18 +0100

    Sames as previous commit

--- init.lua
+++ init.lua
@@ -22,3 +22,15 @@ vim.keymap.set('n', '<leader>h', ':nohlsearch<Cr>')
 -- Enable to copy/paste using system clipboard
 vim.api.nvim_set_option("clipboard","unnamed")
 vim.g.copilot_proxy_strict_ssl = false
+
+-- Automatically format Terraform files on save
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = {'*.tf', '*.tfvars'},  -- Matches Terraform files
+  callback = function()
+    -- Run terraform fmt using a shell command after saving
+    vim.fn.system('terraform fmt ' .. vim.fn.expand('%'))
+    -- Optionally reload the file to reflect changes
+    vim.cmd('edit')
+  end
+})
+
