7acd03c8cf3b54c8e141107fee14fea36076a4a4
Cristian Camilo Morales tapias <criscamota199890@gmail.com>
2024-12-27 23:02:49 +0100

    feat: initial setup for Neovim plugins - Added configuration for nvim-tree with keymaps for toggling and finding files. - Set up Copilot Chat with various prompts and keymaps for code assistance. - Configured lualine with custom separators and sections. - Set up indent-blankline for better code indentation visualization.

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -12,13 +12,16 @@ require("nvim-tree").setup({
 })
 
 -- Keymap to toggle nvim-tree
-vim.keymap.set('n', '<c-n>', ':NvimTreeFindFile<CR>')
+vim.keymap.set('n', '<c-n>', ':NvimTreeToggle<CR>')
+
+-- Open nvim-tree and find the file under the cursor
+vim.keymap.set('n', '<c-f>', ':NvimTreeFindFile<CR>')
 
 -- Autocommand to open nvim-tree on startup and switch focus to the file
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    require("nvim-tree.api").tree.open()
-    -- Switch focus back to the file
-    vim.cmd("wincmd l")
-  end
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   callback = function()
+--     require("nvim-tree.api").tree.open()
+--     -- Switch focus back to the file
+--     vim.cmd("wincmd l")
+--   end
+-- })
